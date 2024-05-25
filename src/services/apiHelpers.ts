325ea4f1b4da94d394715235d421e2f492ae8e1a
RoshanDalami <roshandalami0@gmail.com>
2024-05-25 20:52:36 +0545

    hosted url

--- src/services/apiHelpers.ts
+++ src/services/apiHelpers.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-export const baseurl = 'http://localhost:8000/api/v1';
-// export const baseurl = 'https://hariyali-share-backend.onrender.com/api/v1';
+// export const baseurl = 'http://localhost:8000/api/v1';
+export const baseurl = 'https://hariyali-share-backend.onrender.com/api/v1';
 const token = Cookies.get('token')
 export const mainApi = async(method:string,url:string,data?:any) =>{
     let response = await axios({
