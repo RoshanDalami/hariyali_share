41a2a191c174ccc2c8d5c9a328383ec5be8bdb46
KiranMdhr <kiranmdhr9@gmail.com>
2024-05-28 13:11:03 +0545

    sidbar

--- src/app/(public)/user/page.tsx
+++ src/app/(public)/user/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./Components/Sidebar";
 
 export default function page() {
   return (
     <div>
-      Hello User
+      <Sidebar />
     </div>
-  )
+  );
 }
